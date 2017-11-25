8d8217525228e3fa2cbf17dfcc3892e4d6158bbe
Liz Burton <liz.burton147@gmail.com>
2017-11-25 22:37:34 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,24 +14,24 @@ require 'pry'
   #   }
   # }
 
-# def remove_strawberry(contacts)
-#   contacts.each do | person, data |
-#     if person == "Freddy Mercury"
-#       data.each do | attribute, value |
-#         if attribute == :favorite_ice_cream_flavors
-#           value.delete_if { | flavor | flavor == "strawberry" }
-#         end
-#       end
-#     end
-#   end
-# end
-
 def remove_strawberry(contacts)
-  contacts.each do | person, contact_details_hash |
-    contact_details_hash.each do | attribute, data |
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+  contacts.each do | person, data |
+    if person == "Freddy Mercury"
+      data.each do | attribute, value |
+        if attribute == :favorite_ice_cream_flavors
+          value.delete_if { | flavor | flavor == "strawberry" }
+        end
       end
     end
   end
 end
+
+# def remove_strawberry(contacts)
+#   contacts.each do | person, contact_details_hash |
+#     contact_details_hash.each do | attribute, data |
+#       if attribute == :favorite_ice_cream_flavors
+#         data.delete_if {|ice_cream| ice_cream == "strawberry"}
+#       end
+#     end
+#   end
+# end
