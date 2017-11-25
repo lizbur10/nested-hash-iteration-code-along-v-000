489922c1e9b080796e481942c7f3889eb46bd0ba
Liz Burton <liz.burton147@gmail.com>
2017-11-25 22:49:22 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
-  # This is the array we will be passing into the remove_strawberry method
+#  This is the array we will be passing into the remove_strawberry method
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  #     email: "jon_snow@thewall.we",
+  #     favorite_ice_cream_flavors: ["chocolate", "vanilla", "strawberry"]
   #   },
   #   "Freddy Mercury" => {
   #     name: "Freddy",
@@ -13,7 +13,35 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
-def remove_strawberry(contacts)
 
+
+## Iteration solution -- passes spec
+def remove_strawberry(contacts)
+  contacts.each do | person, data |
+    if person == "Freddy Mercury"
+      data.each do | attribute, value |
+        if attribute == :favorite_ice_cream_flavors
+          value.delete_if { | flavor | flavor == "strawberry" }
+        end
+      end
+    end
+  end
 end
+
+##Direct access solution -- passes spec
+# def remove_strawberry(contacts)
+#   contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete_if { | flavor | flavor == "strawberry" }
+#   contacts
+# end
+
+## Solution from codealong-- passes spec but nonetheless incorrect (removes any instance of strawberry, not just for Freddy Mercury ##
+##
+# def remove_strawberry(contacts)
+#   contacts.each do | person, contact_details_hash |
+#     contact_details_hash.each do | attribute, data |
+#       if attribute == :favorite_ice_cream_flavors
+#         data.delete_if {|ice_cream| ice_cream == "strawberry"}
+#       end
+#     end
+#   end
+# end
